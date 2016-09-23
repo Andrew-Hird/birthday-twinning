1da69d4aea3fb3cd7d0ae85d07daeb1dc2d925ce
Tealiie Tim Andrew <andrew.james.hird@gmail.com>
2016-09-23 16:00:02 +1200

    finished

--- js/index.js
+++ js/index.js
@@ -11,13 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
     loadingDiv.innerHTML = loadingTemplate({loading: true})
 
     let day = document.getElementById('day_id').value
-    console.log(day)
-
     let month = document.getElementById('month_id').value
-    console.log(month)
 
     const url = `https://daxeel-celebinfo-v1.p.mashape.com/date/${day}/${month}`
-    console.log(url)
 
     let html = 'oh an error! ur bday dsnt exst'
     request
@@ -30,7 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
           const resultDiv = document.getElementById('result')
           resultDiv.innerHTML = indexTemplate({result: result})
         }
-
       })
   })
 })

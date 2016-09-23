d3d2fbcc802549b0e97689c2f7c78912a751a61d
Tealiie Tim Andrew <andrew.james.hird@gmail.com>
2016-09-23 14:29:08 +1200

    can submit birthday form

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
 import request from 'superagent'
 import imageTemplate from '../views/result.hbs'
+import formTemplate from '../views/index.hbs'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const url = 'https://daxeel-celebinfo-v1.p.mashape.com/date/23/september'
-  let html = 'oh an error! ur bday dsnt exst'
-  request
-    .get(url)
-    .set('X-Mashape-Key', 'K8PePus3J5mshL1PGPWADznQPwpcp1DrIJ1jsniD7KHEtKWsUT')
-    .end((err, res) => {
-      if(!err) {
-        html = JSON.parse(res.text).data[0].age
-        console.log(JSON.parse(res.text).data)
-      }
-        document.getElementById('result').innerHTML = html
-    })
+  const div = document.createElement('div')
+  div.innerHTML = formTemplate({})
+  document.body.appendChild(div)
+
+  document.getElementById("submit").addEventListener("click", () => {
+    let day = document.getElementById('day_id').value
+    console.log(day)
+
+    let month = document.getElementById('month_id').value
+    console.log(month)
+
+    const url = `https://daxeel-celebinfo-v1.p.mashape.com/date/${day}/${month}`
+    console.log(url)
+
+    let html = 'oh an error! ur bday dsnt exst'
+    request
+      .get(url)
+      .set('X-Mashape-Key', 'K8PePus3J5mshL1PGPWADznQPwpcp1DrIJ1jsniD7KHEtKWsUT')
+      .end((err, res) => {
+        if(!err) {
+          html = JSON.parse(res.text).data[0].age
+          console.log(JSON.parse(res.text).data)
+        }
+          document.getElementById('result').innerHTML = html
+      })
+  })
+
 })
